2c7c703dfc8e10c6880b33fb4dfcf1bca9f440d2
OJ188 <44781111+OJ188@users.noreply.github.com>
2019-04-24 13:58:52 +0200

    update violin, not finished

--- Weed-Veez.R
+++ Weed-Veez.R
@@ -10,7 +10,8 @@ library(tidyverse)
 library(dygraphs)
 library(xts)          # To make the convertion data-frame / xts format
 library(lubridate)
-
+library(rlist)
+library(treemap)
 ###### OPENING FILE
 df = read.table("Weed_Price.csv",header=TRUE,sep=",",dec=".",stringsAsFactors = TRUE)
 attach(df)
@@ -173,26 +174,60 @@ any(is.na(data))
 
 
 
-States_list = c("Indiana","Iowa","Wyoming","Mississippi")
-df_violin = data.frame(subset(df, State == "Alabama"))
+States_list = c("Alabama","Indiana","Iowa","Wyoming","Mississippi")
+# States_list = c("Indiana","Iowa","Wyoming","Mississippi")
+# list_test = list()
+# df_violin = data.frame(subset(df, State == "Alabama"))
+# df_violin <- data.frame(State=character(),
+                 # HighQ=double(),
+                 # MedQ=double(),
+                 # LowQ=double(),
+                 # date=as.Date(character()), 
+                 # stringsAsFactors=FALSE) 
+State_list = data.frame(State=character(),stringsAsFactors = FALSE)
+HighQ_list = data.frame(HighQ=double(),stringsAsFactors = FALSE)
+MedQ_list = data.frame(MedQ=double(),stringsAsFactors = FALSE)
+LowQ_list = data.frame(LowQ=double(),stringsAsFactors = FALSE)
+date_list = data.frame(date=as.Date(character()),stringsAsFactors = FALSE)
+                       
 for(i in States_list)
 {
-  df_violin = cbind(df_violin,subset(df, State == i))
+  # df_violin = cbind(df_violin,subset(df, State == i))
+  df_violin = subset(df, State == i)
+  State_list = rbind(State_list,df_violin[1])
+  HighQ_list = rbind(HighQ_list,df_violin[2])
+  MedQ_list = rbind(MedQ_list,df_violin[4])
+  LowQ_list = rbind(LowQ_list,df_violin[6])
+  date_list = rbind(date_list,df_violin[8])
+  # df_violin = df_violin[with(df_violin, order(df_violin$State,df_violin$date)),]
+  # na.locf(df_violin)
+  # State_list = list.append(State_list,df_violin[1])
+  # HighQ_list = list.append(HighQ_list,df_violin[2])
+  # MedQ_list = list.append(MedQ_list,df_violin[4])
+  # LowQ_list = list.append(LowQ_list,df_violin[6])
+  # date_list = list.append(date_list,df_violin[8])
+  # list_test = list.append(list_test,subset(df, State == i))
 }
 # Mean_state = data.frame(aggregate(MedQ~State, data=df, 
                                   # FUN=function(df) c(mean=mean(df), count=length(df))))
 
 # df_violin=df_violin[order(Violin[,2]),]
+
+df_violin = cbind(State_list,HighQ_list,MedQ_list,LowQ_list,date_list)
+names_list = c("State","HighQ","MedQ","LowQ","date")
+names(df_violin) = names_list
 attach(df_violin)
-MedQ_violin = MedQ[1:10]
-State_violin = State[1:10]
-Q = HighQ[1:10]
+State = as.character(State)
+
+
+# Q = df_violin[, grepl("\\bHighQ\\b", names(df_violin))]
+Q = HighQ
 
 
 ###### Violin plot
 df %>%
-  mutate(State = fct_reorder(State, Q, .fun = median)) %>%
-  ggplot(aes(x=State, y=Q, fill=State)) +
+  mutate(date = fct_reorder(date, Q, .fun = median)) %>%
+  ggplot(aes(x=date, y=Q, fill=date)) +
   geom_violin() +
   geom_jitter(color="grey", width=.2, size=.9, alpha=.8) +
   theme(
@@ -211,7 +246,6 @@ df %>%
 df1=read.table("Weed_Price.csv",header=TRUE,sep=",",dec=".")
 df2=df1[c(1:51),]
 df2$date <- as.Date(df2$date)
-library(treemap)
 
 treemap(df2,
         index="State",
